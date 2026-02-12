59ce8d36f35e3ae9e90b118e7fc9a611c9f2e790
Ganesh <ganeshmano1042@gmail.com>
2026-02-12 14:54:15 +0530

    Reverse a String

---
+++ ReverseTheString.java
@@ -0,0 +1,17 @@
+public class Solution5
+{
+    static String reverse(String s)
+    {
+        if(s.isEmpty())
+        {
+            return s;
+        }
+        return reverse(s.substring(1))+(s.charAt(0));
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(reverse("Ganesh"));
+    }
+}
