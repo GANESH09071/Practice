36397ed7cd8fe5829f9974f1e3f636b5db16f6be
Ganesh <ganeshmano1042@gmail.com>
2026-02-15 21:32:31 +0530

    Roman to Integer

---
+++ RomantoInteger.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class RomantoInteger 
+{
+     public static void main(String[] args) 
+  {
+    String s ="LVIII";
+    System.out.println(romanToInt(s));
+
+  }
+    public static int romanToInt(String s) 
+    {
+     HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+
+            if(i < s.length()-1 && value < map.get(s.charAt(i+1)))
+                total -= value;
+            else
+                total += value;
+        }
+        return total;
+        
+    }
+}
+    
+
